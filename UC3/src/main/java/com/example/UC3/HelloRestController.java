2b7cf24bd567f4843071e7ae5198979c884a52f4
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-22 13:36:34 +0530

    [Kanishk] Add. Create hello world app to display REST call

---
+++ UC3/src/main/java/com/example/UC3/HelloRestController.java
@@ -0,0 +1,12 @@
+package com.example.UC3;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from BridgeLabz";
+    }
+}
